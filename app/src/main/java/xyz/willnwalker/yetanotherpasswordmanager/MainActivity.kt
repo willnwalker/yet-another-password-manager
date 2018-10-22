aef56613e4441e5fc76aec4751e66f96a332e846
William Walker <willnwalker@outlook.com>
2018-10-21 18:17:10 -0700

    Fixed UI controller imports.

--- app/src/main/java/xyz/willnwalker/yetanotherpasswordmanager/MainActivity.kt
+++ app/src/main/java/xyz/willnwalker/yetanotherpasswordmanager/MainActivity.kt
@@ -11,6 +11,7 @@ import android.view.MenuItem
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
