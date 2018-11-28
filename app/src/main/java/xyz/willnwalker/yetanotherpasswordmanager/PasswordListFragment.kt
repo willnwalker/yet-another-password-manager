a02c5f931b5d1f7b8a52a25471960e1ce67a3fde
William Walker <willnwalker@outlook.com>
2018-11-28 12:19:42 -0800

    Code Cleanup & Build system updates
    --Refactored LoginFragment so that class name matches file name
    --Removed more boilerplate code, Fragments down to less than 200 lines of code each
    --Upgraded Gradle files to use Gradle 3.3.0 release candidate

--- app/src/main/java/xyz/willnwalker/yetanotherpasswordmanager/PasswordListFragment.kt
+++ app/src/main/java/xyz/willnwalker/yetanotherpasswordmanager/PasswordListFragment.kt
@@ -1,7 +1,6 @@
 package xyz.willnwalker.yetanotherpasswordmanager
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -17,37 +16,18 @@ import kotlinx.android.synthetic.main.fragment_password_list.*
 
 /**
  * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [PasswordListFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [PasswordListFragment.newInstance] factory method to
- * create an instance of this fragment.
+ *
  */
 class PasswordListFragment : Fragment() {
-
-    // TODO: Rename and change types of parameters
-    private var mParam1: String? = null
-    private var mParam2: String? = null
-
-    private var mListener: OnFragmentInteractionListener? = null
 
     private lateinit var contextConfirmed : Context
 
     // Kyle: initialize linearLayoutManager
     private lateinit var linearLayoutManager: LinearLayoutManager
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (arguments != null) {
-            mParam1 = arguments!!.getString(ARG_PARAM1)
-            mParam2 = arguments!!.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_password_list, container, false)
     }
 
@@ -55,9 +35,6 @@ class PasswordListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         fab.setOnClickListener {
             findNavController(it).navigate(R.id.action_new_password)
-//            val bundle = Bundle()
-//            bundle.putString("uuid", "NEW_PASSWORD")
-//            findNavController(it).navigate(R.id.action_new_password, bundle)
         }
 
         linearLayoutManager = LinearLayoutManager(activity)
@@ -65,9 +42,9 @@ class PasswordListFragment : Fragment() {
         // Kyle: adds a horizontal line separator between each item
         passwordList.addItemDecoration(PasswordListItemDecoration(contextConfirmed, 40, 40))
 
-        var config: RealmConfiguration = RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build()
-        var realm = Realm.getInstance(config)
-        var entries = realm.where<Entry>().findAllAsync()
+        val config: RealmConfiguration = RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build()
+        val realm = Realm.getInstance(config)
+        val entries = realm.where<Entry>().findAllAsync()
         passwordList.setAdapter(PasswordListAdapter(contextConfirmed, entries, true, false, ""))
 
     }
@@ -78,54 +55,4 @@ class PasswordListFragment : Fragment() {
         contextConfirmed = _context
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        if (mListener != null) {
-            mListener!!.onFragmentInteraction(uri)
-        }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        mListener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
-        private val ARG_PARAM2 = "param2"
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PasswordListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        fun newInstance(param1: String, param2: String): PasswordListFragment {
-            val fragment = PasswordListFragment()
-            val args = Bundle()
-            args.putString(ARG_PARAM1, param1)
-            args.putString(ARG_PARAM2, param2)
-            fragment.arguments = args
-            return fragment
-        }
-    }
-}// Required empty public constructor
+}
--- app/src/main/java/xyz/willnwalker/yetanotherpasswordmanager/PasswordViewFragment.kt
+++ app/src/main/java/xyz/willnwalker/yetanotherpasswordmanager/PasswordViewFragment.kt
@@ -1,6 +1,5 @@
 package xyz.willnwalker.yetanotherpasswordmanager
 
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.text.InputType
@@ -8,31 +7,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation.findNavController
-
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import kotlinx.android.synthetic.main.fragment_password_view.*
 import java.security.NoSuchAlgorithmException
 import java.security.SecureRandom
-
 import com.afollestad.materialdialogs.MaterialDialog
 import io.realm.kotlin.where
 
 
 /**
  * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [PasswordViewFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [PasswordViewFragment.newInstance] factory method to
- * create an instance of this fragment.
+ *
  */
 class PasswordViewFragment : Fragment() {
-
-    // TODO: Rename and change types of parameters
-
-    private var mListener: OnFragmentInteractionListener? = null
-
     private lateinit var config: RealmConfiguration
     private lateinit var realm : Realm
 
@@ -106,13 +94,6 @@ class PasswordViewFragment : Fragment() {
         }
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        if (mListener != null) {
-            mListener!!.onFragmentInteraction(uri)
-        }
-    }
-
     fun genPassword(length: Int, specialChars: Boolean): String {
         val range: Int
         var pass = ""
@@ -147,47 +128,4 @@ class PasswordViewFragment : Fragment() {
         return num.toChar()
     }
 
-    override fun onDetach() {
-        super.onDetach()
-        mListener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
-     */
-    interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
-    }
-
-    companion object {
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
-        private val ARG_PARAM2 = "param2"
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PasswordViewFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        fun newInstance(param1: String, param2: String): PasswordViewFragment {
-            val fragment = PasswordViewFragment()
-            val args = Bundle()
-            args.putString(ARG_PARAM1, param1)
-            args.putString(ARG_PARAM2, param2)
-            fragment.arguments = args
-            return fragment
-        }
-    }
-}// Required empty public constructor
+}
