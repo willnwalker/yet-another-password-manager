7ad2c815f1b987e27ec78e60f3bb1a72a4e300cc
William Walker <willnwalker@outlook.com>
2019-07-30 15:59:56 -0700

    Encryption changes (WIP)
    Disabled fingerprint auth, added code to set PIN using Venmo library

--- app/src/main/java/xyz/willnwalker/yetanotherpasswordmanager/MainActivity.kt
+++ app/src/main/java/xyz/willnwalker/yetanotherpasswordmanager/MainActivity.kt
@@ -6,13 +6,18 @@ import android.preference.PreferenceManager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import kotlinx.android.synthetic.main.activity_main.*
+import com.venmo.android.pin.PinFragment
+import com.venmo.android.pin.util.PinHelper
 
-class MainActivity : AppCompatActivity(), UIListener{
+
+
+class MainActivity : AppCompatActivity(), UIListener, PinFragment.Listener{
 
     private lateinit var prefs : SharedPreferences
     private lateinit var nav : NavController
@@ -40,23 +45,23 @@ class MainActivity : AppCompatActivity(), UIListener{
         firstRun = prefs.getBoolean("firstRun", true)
         securityEnabled = prefs.getBoolean("securityEnabled", false)
 
-        nav.navigate(R.id.loadingFragment)
-
-        when{
-            firstRun || securityEnabled -> nav.navigate(R.id.loginFragment)
-            else -> nav.navigate(R.id.passwordListFragment)
-        }
+//        nav.navigate(R.id.loadingFragment)
+//
+//        when{
+//            firstRun || securityEnabled -> nav.navigate(R.id.loginFragment)
+//            else -> nav.navigate(R.id.passwordListFragment)
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
-        if(!firstRun){
+//        if(!firstRun){
             if(!securityEnabled){
                 menu.findItem(R.id.action_settings).title = "Enable Encryption"
             }
             //TODO: Set Settings action to decrypt if security enabled
-        }
+//        }
         return true
     }
 
@@ -66,7 +71,15 @@ class MainActivity : AppCompatActivity(), UIListener{
         // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
             R.id.action_settings -> {
-                nav.navigate(R.id.loginFragment)
+//                nav.navigate(R.id.loginFragment)
+                val toShow = if (PinHelper.hasDefaultPinSaved(this))
+                    PinFragment.newInstanceForVerification()
+                else
+                    PinFragment.newInstanceForCreation()
+
+                fragmentManager.beginTransaction()
+                        .replace(R.id.nav_host, toShow)
+                        .commit()
                 return true
             }
             else -> return super.onOptionsItemSelected(item)
@@ -83,5 +96,14 @@ class MainActivity : AppCompatActivity(), UIListener{
 
     override fun exit(){
         finish()
+    }
+
+    override fun onPinCreated() {
+        nav.popBackStack()
+    }
+
+    override fun onValidated() {
+        Toast.makeText(this,"Correct PIN!", Toast.LENGTH_SHORT).show()
+        nav.popBackStack()
     }
 }
