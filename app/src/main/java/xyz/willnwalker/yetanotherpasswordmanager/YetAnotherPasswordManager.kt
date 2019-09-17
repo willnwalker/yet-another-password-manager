bf746a6cafc9f765889e4753f4caf1907b779f92
William Walker <willnwalker@outlook.com>
2019-09-17 14:33:17 -0700

    Added annotation to suppress warnings for unused application class

--- app/src/main/java/xyz/willnwalker/yetanotherpasswordmanager/YetAnotherPasswordManager.kt
+++ app/src/main/java/xyz/willnwalker/yetanotherpasswordmanager/YetAnotherPasswordManager.kt
@@ -3,6 +3,7 @@ package xyz.willnwalker.yetanotherpasswordmanager
 import android.app.Application
 import com.facebook.stetho.Stetho
 
+@Suppress("unused")
 class YetAnotherPasswordManager : Application() {
 
     override fun onCreate() {
